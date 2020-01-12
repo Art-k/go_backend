7497c13ccc26268492886294a88f87db09f95d90
kryhin <artem@maxtvmedia.com>
2020-01-13 00:03:34 +0200

    ReverseRelay

--- src/rule.go
+++ src/rule.go
@@ -141,19 +141,26 @@ func ActionRule(w http.ResponseWriter, r *http.Request) {
 		if Type == "timer" {
 			var rule RuleByTimer
 			Db.Where("id = ?", id).Delete(&rule)
+
+			addedRecordString, _ := json.Marshal(rule)
+			w.Header().Set("content-type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			n, _ := fmt.Fprintf(w, string(addedRecordString))
+			fmt.Println(n)
+			return
 		}
 
 		if Type == "sensor" {
 			var rule RuleBySensor
 			Db.Where("id = ?", id).Delete(&rule)
-		}
 
-		addedRecordString, _ := json.Marshal(rule)
-		w.Header().Set("content-type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		n, _ := fmt.Fprintf(w, string(addedRecordString))
-		fmt.Println(n)
-		return
+			addedRecordString, _ := json.Marshal(rule)
+			w.Header().Set("content-type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			n, _ := fmt.Fprintf(w, string(addedRecordString))
+			fmt.Println(n)
+			return
+		}
 
 	default:
 	}
